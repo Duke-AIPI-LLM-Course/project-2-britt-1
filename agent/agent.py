5c44015f0cdd092e4d69435eddbaba2e5116687e
Bal67 <bal67@duke.edu>
2025-04-15 18:41:53 -0500

    agent fix

--- agent/agent.py
+++ agent/agent.py
@@ -1,13 +1,14 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from langchain.agents import Tool
 from langchain.agents import initialize_agent, AgentType
 from langchain.tools import Tool
 from tools.rag_tool import ai_meng_rag_tool
 from tools.events_tool import get_duke_events
 from tools.pratt_tool import get_pratt_facts
 from agent.llm_chatbot import replicate_llm  
-import sys
-import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 
 
 tools = [
